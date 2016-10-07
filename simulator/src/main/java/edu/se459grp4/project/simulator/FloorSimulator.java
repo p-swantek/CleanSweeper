3133d90be650d82ce33f101927ef2087df1d91ba
Jerry Kobeszko <jerry.kobeszko@vivakinervecenter.com>
2016-10-07 13:37:07 -0500

    Adding cleansweep dependency to simulator

--- simulator/src/main/java/edu/se459grp4/project/simulator/FloorSimulator.java
+++ simulator/src/main/java/edu/se459grp4/project/simulator/FloorSimulator.java
@@ -1,7 +1,10 @@
 package edu.se459grp4.project.simulator;
 
+import edu.se459grp4.project.cleansweep.CleanSweep;
+
 public class FloorSimulator {
     public static void main(String[] args) {
         System.out.println("HELLO!");
+        CleanSweep cleanSweep = new CleanSweep();
     }
 }
